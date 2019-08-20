ac012fde28e2e708c0c27d5c214027f6126f7a39
HarperT <harper.thurlow@my.jcu.edu.au>
2019-08-20 13:16:46 +0930

    Change broken_score.py to use a function to determine what the result of the score is.

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -3,16 +3,25 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-elif score < 50:
-    print("Bad")
+
+def main():
+    score = float(input("Enter score: "))
+    result = determine_score(score)
+    print(result)
 
 
+def determine_score(score):
+    result = ""
+    if score < 0 or score > 100:
+        result = "Invalid score"
+    elif score >= 90:
+        result = "Excellent"
+    elif score >= 50:
+        result = "Passable"
+    elif score < 50:
+        result = "bad"
+
+    return result
 
 
+main()
