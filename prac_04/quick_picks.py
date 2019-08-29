34816663e95b653dbde636497ff6cf22081b79c4
HarperT <harper.thurlow@my.jcu.edu.au>
2019-08-29 12:49:37 +0930

    create for loops to generate the quick pick

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -5,7 +5,22 @@ MIN_NUMBER = 1
 MAX_NUMBER = 45
 MAX_PICKS = 6
 
-lines = int(input("How many lines? >"))
 
-for line in range(lines):
-    pass
+def main():
+
+    lines = int(input("How many lines? >"))
+    while lines <= 0:
+        print("Please enter a valid number of lines")
+        lines = int(input("How many lines? >"))
+
+    for line in range(lines):
+        quick_pick = []
+        for i in range(MAX_PICKS):
+            number = randint(MIN_NUMBER, MAX_NUMBER)
+            quick_pick.append(number)
+        quick_pick.sort()
+
+        print(" ".join("{:2}".format(number) for number in quick_pick))
+
+
+main()
