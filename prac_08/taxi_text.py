92aed97c75763a02daf0cce28dee05a68fbba83a
jc471891 <harper.thurlow@my.jcu.edu.au>
2019-10-08 16:56:50 +1000

    create taxi class for prac_08

---
+++ prac_08/taxi_text.py
@@ -0,0 +1,13 @@
+from prac_08.taxi import Taxi
+
+
+def main():
+    prius1 = Taxi("Prius 1", 100)
+    prius1.drive(40)
+    print(prius1)
+    prius1.start_fare()
+    prius1.drive(100)
+    print(prius1)
+
+
+main()
