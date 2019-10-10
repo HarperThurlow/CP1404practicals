e87135f079e81fe7368100ff02223895a74a1d17
jc471891 <harper.thurlow@my.jcu.edu.au>
2019-10-10 12:59:52 +1000

    prac_08 inheritence

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -5,19 +5,48 @@ MENU = """q)uit, c)hoose taxi, d)rive"""
 
 
 def main():
+    bill_to_date = 0
+    current_taxi = None
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    print(MENU)
     option = input(">>>").upper()
     while option != "Q":
         if option == "C":
-            choose_taxi()
+            current_taxi = choose_taxi(taxis)
+            print(current_taxi)
         elif option == "D":
-            drive_taxi()
+            trip_cost = drive_taxi(current_taxi)
+            bill_to_date += trip_cost
         else:
             print("Invalid option")
 
+        print("Bill to date: ${:.2f}".format(bill_to_date))
+        print(MENU)
+        option = input(">>>").upper()
+    print("Total trip cost: ${}\nTaxis are now:".format(bill_to_date))
+    print_taxis(taxis)
 
-def choose_taxi():
-    pass
+
+def choose_taxi(taxis):
+    print_taxis(taxis)
+    taxi_choice = int(input("Choose taxi: "))
+    return taxis[taxi_choice]
 
 
-def drive_taxi():
-    pass
+def print_taxis(taxis):
+    for i, taxi in enumerate(taxis):
+        print("{} - {}".format(i, taxi))
+
+
+def drive_taxi(current_taxi):
+    if current_taxi is None:
+        print("Please choose a taxi first")
+        return 0
+    current_taxi.start_fare()
+    drive_distance = int(input("Drive how far? "))
+    current_taxi.drive(drive_distance)
+    print("Your {} trip cost you ${}".format(current_taxi.name, current_taxi.get_fare()))
+    return current_taxi.get_fare()
+
+
+main()
--- prac_08/unreliablecar_test.py
+++ prac_08/unreliablecar_test.py
@@ -2,9 +2,11 @@ from prac_08.unreliablecar import UnreliableCar
 
 
 def main():
-    bad_car = UnreliableCar("Bad", 100, 25)
-    bad_car.drive(50)
-    print(bad_car.odometer)
+
+    bad_car = UnreliableCar("Bad", 100, 50)
+    for i in range(10):
+        bad_car.drive(10)
+        print(bad_car.odometer)
 
 
 main()
